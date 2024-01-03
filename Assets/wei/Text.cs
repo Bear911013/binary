7778724382efa1a1fd5266265fea0c1cee56a1be
Bbear <wang911013@gmail.com>
2024-01-03 13:21:33 +0800

    20240103
    20240103 程式基礎教學

---
+++ Assets/wei/Text.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    void Awake()//初始狀態，程式開啟必執行一次
+    {
+        Debug.Log("Hello");
+    }
+    // Start is called before the first frame update
+    void Start()//初始狀態，物件開啟執行一次
+    {
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
